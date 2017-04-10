279c481c5f63f2c5650c97a52a97626edea03a2b
Hudson Nicoletti <hudson19962009@gmail.com>
2017-04-10 15:36:09 -0300

    gallery fix

--- apps/Website/controllers/IndexController.php
+++ apps/Website/controllers/IndexController.php
@@ -3,11 +3,13 @@
 namespace Website\Controllers;
 
 use Api\Models\Tips as Tips;
+use Api\Models\Gallery   as Gallery;
 
 class IndexController extends ControllerBase
 {
   public function IndexAction()
   {
+    $this->view->gallery     = Gallery::find();
     $this->view->tips = Tips::find(["order"=>"_ DESC"]);
   }
   public function AboutAction()
